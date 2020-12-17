d7c1662a4822424d90cb7ddec396b8774d27ec8e
francischempa <francis.chempa@icloud.com>
2020-12-17 08:07:35 +0000

    CallSite, ClassLoaderInfo integration. filter out $$Lambda frames

--- cmdLine/src/main/scala/topgun/cmdline/EventTypeIdMapping.scala
+++ cmdLine/src/main/scala/topgun/cmdline/EventTypeIdMapping.scala
@@ -2,13 +2,9 @@ package topgun.cmdline
 
 // TODO find reference values inside JMC/JFR and replace with constants.
 //  RecrodedEvent.AllocatedInNewTLAB
-object RecordedEventTypeIdMapping {
+object EventTypeIdMapping {
   val AllocationInNewTLAB = 331
   val AllocationOutsideTlab = 332
   val MethodProfilingSample = 352
   val MethodProfilingSampleNative = 353
-  val RecordingSetting = 2999
-  val OSInformation = 337
-  val InitialSystemProperty = 338
-
 }
