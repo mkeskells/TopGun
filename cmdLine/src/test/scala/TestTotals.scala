0a753b35b9d6467302ba5b9a8ffd9314feb84506
Afi <afimaamedufie@gmail.com>
2020-12-30 13:51:02 +0000

    Added basic tests for Aggregate View

--- cmdLine/src/test/scala/TestTotals.scala
+++ cmdLine/src/test/scala/TestTotals.scala
@@ -15,10 +15,10 @@ class TestTotals {
     options.validate(parser)
   }
 
-  //Getting the Absolute Path for the jfr file.
 
+  //Getting the Absolute Path for the jfr file.
   private def getAbsolutePath = {
-    val res = getClass.getClassLoader.getResource("jfr_files/flight_recording_1108comintellijideaMain4564_2020-11-30_12-48-18_ae084800.jfr")
+    val res = getClass.getClassLoader.getResource("jfr_file/scala.jfr")
     val file = Paths.get(res.toURI).toFile
     file.getAbsolutePath
   }
@@ -27,15 +27,17 @@ class TestTotals {
 
   //Test for the Totals of Events
   @Test def testTotals(): Unit = {
+
     val totals = new Totals
     new FileParser(options.jfr, options, totals, new Configuration(options)).parse()
-    Assert.assertEquals(134, totals.consumedCpuEvents.get)
+    Assert.assertEquals(987, totals.consumedCpuEvents.get)
     Assert.assertEquals(0, totals.ignoredThreadAllocationEvents.get)
     Assert.assertEquals(0, totals.ignoredStackAllocationEvents.get)
     Assert.assertEquals(0, totals.ignoredStackCpuEvents.get)
-    Assert.assertEquals(1722, totals.consumedAllocationEvents.get)
+    Assert.assertEquals(98503, totals.consumedAllocationEvents.get)
     Assert.assertEquals(0, totals.ignoredThreadCpuEvents.get)
-    Assert.assertEquals(41273, totals.ignoredEvents.get)
-    Assert.assertEquals(46072, totals.totalEvents.get)
+    Assert.assertEquals(58009, totals.ignoredEvents.get)
+    Assert.assertEquals(169187, totals.totalEvents.get)
+
   }
 }
